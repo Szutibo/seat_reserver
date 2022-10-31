eeb1002750bc672bb7fe68e596ec519804c06f75
Szűcs Tibor <szutibo@gmail.com>
2022-10-31 18:03:28 +0100

    The app is finished

---
+++ app.js
@@ -0,0 +1,560 @@
+let seats = [
+    {
+        position: 'auditorium',
+        row: 'A',
+        seatNumber: 1,
+        tier: 1,
+        reserved: false,
+        id: 1,
+    },
+    {
+        position: 'auditorium',
+        row: 'A',
+        seatNumber: 2,
+        tier: 1,
+        reserved: false,
+        id: 2,
+    },
+    {
+        position: 'auditorium',
+        row: 'A',
+        seatNumber: 3,
+        tier: 1,
+        reserved: false,
+        id: 3,
+    },
+    {
+        position: 'auditorium',
+        row: 'A',
+        seatNumber: 4,
+        tier: 1,
+        reserved: false,
+        id: 4,
+    },
+    {
+        position: 'auditorium',
+        row: 'A',
+        seatNumber: 5,
+        tier: 1,
+        reserved: false,
+        id: 5,
+    },
+    {
+        position: 'auditorium',
+        row: 'A',
+        seatNumber: 6,
+        tier: 1,
+        reserved: false,
+        id: 6,
+    },
+    {
+        position: 'auditorium',
+        row: 'B',
+        seatNumber: 1,
+        tier: 2,
+        reserved: false,
+        id: 7,
+    },
+    {
+        position: 'auditorium',
+        row: 'B',
+        seatNumber: 2,
+        tier: 1,
+        reserved: false,
+        id: 8,
+    },
+    {
+        position: 'auditorium',
+        row: 'B',
+        seatNumber: 3,
+        tier: 1,
+        reserved: false,
+        id: 9,
+    },
+    {
+        position: 'auditorium',
+        row: 'B',
+        seatNumber: 4,
+        tier: 1,
+        reserved: false,
+        id: 10,
+    },
+    {
+        position: 'auditorium',
+        row: 'B',
+        seatNumber: 5,
+        tier: 1,
+        reserved: false,
+        id: 11,
+    },
+    {
+        position: 'auditorium',
+        row: 'B',
+        seatNumber: 6,
+        tier: 1,
+        reserved: false,
+        id: 12,
+    },
+    {
+        position: 'auditorium',
+        row: 'B',
+        seatNumber: 7,
+        tier: 2,
+        reserved: false,
+        id: 13,
+    },
+    {
+        position: 'auditorium',
+        row: 'C',
+        seatNumber: 1,
+        tier: 2,
+        reserved: false,
+        id: 14,
+    },
+    {
+        position: 'auditorium',
+        row: 'C',
+        seatNumber: 2,
+        tier: 2,
+        reserved: false,
+        id: 15,
+    },
+    {
+        position: 'auditorium',
+        row: 'C',
+        seatNumber: 3,
+        tier: 1,
+        reserved: false,
+        id: 16,
+    },
+    {
+        position: 'auditorium',
+        row: 'C',
+        seatNumber: 4,
+        tier: 1,
+        reserved: false,
+        id: 17,
+    },
+    {
+        position: 'auditorium',
+        row: 'C',
+        seatNumber: 5,
+        tier: 2,
+        reserved: false,
+        id: 18,
+    },
+    {
+        position: 'auditorium',
+        row: 'C',
+        seatNumber: 6,
+        tier: 2,
+        reserved: false,
+        id: 19,
+    },
+    {
+        position: 'auditorium',
+        row: 'D',
+        seatNumber: 1,
+        tier: 3,
+        reserved: false,
+        id: 20,
+    },
+    {
+        position: 'auditorium',
+        row: 'D',
+        seatNumber: 2,
+        tier: 2,
+        reserved: false,
+        id: 21,
+    },
+    {
+        position: 'auditorium',
+        row: 'D',
+        seatNumber: 3,
+        tier: 2,
+        reserved: false,
+        id: 22,
+    },
+    {
+        position: 'auditorium',
+        row: 'D',
+        seatNumber: 4,
+        tier: 2,
+        reserved: false,
+        id: 23,
+    },
+    {
+        position: 'auditorium',
+        row: 'D',
+        seatNumber: 5,
+        tier: 3,
+        reserved: false,
+        id: 24,
+    },
+    {
+        position: 'auditorium',
+        row: 'E',
+        seatNumber: 1,
+        tier: 3,
+        reserved: false,
+        id: 25,
+    },
+    {
+        position: 'auditorium',
+        row: 'E',
+        seatNumber: 2,
+        tier: 3,
+        reserved: false,
+        id: 26,
+    },
+    {
+        position: 'auditorium',
+        row: 'E',
+        seatNumber: 3,
+        tier: 3,
+        reserved: false,
+        id: 27,
+    },
+    {
+        position: 'auditorium',
+        row: 'E',
+        seatNumber: 4,
+        tier: 3,
+        reserved: false,
+        id: 28,
+    },
+    {
+        position: 'balcony',
+        row: 'A',
+        seatNumber: 1,
+        tier: 2,
+        reserved: false,
+        id: 29,
+    },
+    {
+        position: 'balcony',
+        row: 'A',
+        seatNumber: 2,
+        tier: 2,
+        reserved: false,
+        id: 30,
+    },
+    {
+        position: 'balcony',
+        row: 'A',
+        seatNumber: 3,
+        tier: 1,
+        reserved: false,
+        id: 31,
+    },
+    {
+        position: 'balcony',
+        row: 'A',
+        seatNumber: 4,
+        tier: 1,
+        reserved: false,
+        id: 32,
+    },
+    {
+        position: 'balcony',
+        row: 'A',
+        seatNumber: 5,
+        tier: 1,
+        reserved: false,
+        id: 33,
+    },
+    {
+        position: 'balcony',
+        row: 'A',
+        seatNumber: 6,
+        tier: 2,
+        reserved: false,
+        id: 34,
+    },
+    {
+        position: 'balcony',
+        row: 'A',
+        seatNumber: 7,
+        tier: 2,
+        reserved: false,
+        id: 35,
+    },
+    {
+        position: 'balcony',
+        row: 'B',
+        seatNumber: 1,
+        tier: 3,
+        reserved: false,
+        id: 36,
+    },
+    {
+        position: 'balcony',
+        row: 'B',
+        seatNumber: 2,
+        tier: 2,
+        reserved: false,
+        id: 37,
+    },
+    {
+        position: 'balcony',
+        row: 'B',
+        seatNumber: 3,
+        tier: 2,
+        reserved: false,
+        id: 38,
+    },
+    {
+        position: 'balcony',
+        row: 'B',
+        seatNumber: 4,
+        tier: 2,
+        reserved: false,
+        id: 39,
+    },
+    {
+        position: 'balcony',
+        row: 'B',
+        seatNumber: 5,
+        tier: 2,
+        reserved: false,
+        id: 40,
+    },
+    {
+        position: 'balcony',
+        row: 'B',
+        seatNumber: 6,
+        tier: 3,
+        reserved: false,
+        id: 41,
+    },
+];
+var numberOfReservedSeats = document.getElementById('occupied-seats-range'); // Min 20% max 100%
+var numberOfDemandedSeats = document.getElementById('demanded-seats-range'); // Min 1 max 4
+const reservedSeatsIndexes = []; // Randomly generated index numbers
+let errors = [];
+let bestOption = [];
+const occupiedSeatsCounter = document.getElementById('occupied-seats');
+const demandedSeatsCounter = document.getElementById('demanded-seats');
+const errorContainer = document.getElementById('error-container');
+const resultContainer = document.getElementById('result-container');
+const result = document.getElementById('result-items');
+const searchBtn = document.getElementById('search-btn');
+
+// GUI related functions
+demandedSeatsCounter.innerHTML = numberOfDemandedSeats.value;
+occupiedSeatsCounter.innerHTML = numberOfReservedSeats.value;
+
+numberOfDemandedSeats.addEventListener('change', () => {
+    demandedSeatsCounter.innerHTML = numberOfDemandedSeats.value;
+});
+
+numberOfReservedSeats.addEventListener('change', () => {
+    occupiedSeatsCounter.innerHTML = numberOfReservedSeats.value;
+});
+
+searchBtn.addEventListener('click', () => {
+    errors = [];
+    reserveRandomSeats();
+    findFreeSeats();
+    if (errors.length > 0) {
+        resultContainer.style.display = 'none';
+        result.innerHTML = '';
+        errorContainer.style.display = 'block';
+        errorContainer.innerHTML = errors;
+    } else {
+        errorContainer.style.display = 'none';
+        errorContainer.innerHTML = '';
+        resultContainer.style.display = 'block';
+        result.innerHTML = createResults(bestOption).join('');
+    }
+});
+
+// Seat reservation related functions
+function reserveRandomSeats() {
+    if (numberOfReservedSeats.value < Math.floor(seats.length * 0.2)) {
+        errors = `Reserved seats' number cannot be less than ${Math.floor(seats.length * 0.2)}`;
+    } else if (numberOfReservedSeats.value > seats.length) {
+        errors = `Reserved seats' number cannot be more than ${seats.length}`;
+    } else {
+        // Creating an array filled with random indexes based on how many reserved seats we want
+        for (let i = 0; i < numberOfReservedSeats.value;) {
+            let randomIndex = Math.floor(Math.random() * seats.length);
+
+            if (reservedSeatsIndexes.indexOf(randomIndex) === -1) {
+                reservedSeatsIndexes.push(randomIndex);
+                i++;
+            };
+        };
+
+        // Setting seats' reserved prop to true in the original array based on the random generated array of indexes
+        reservedSeatsIndexes.forEach(index => {
+            seats[index].reserved = true;
+        });
+    }
+};
+
+function resetArray() {
+    let reservedSeats = seats.filter(seat => seat.reserved);
+    reservedSeatsIndexes.length = 0;
+
+    reservedSeats.forEach(seat => {
+        seat.reserved = false;
+    });
+};
+
+function createResults(object) {
+    let resultArray = [];
+
+    object.forEach(oneObject => {
+        resultArray.push(`<li class="result-item"><span>Zone: </span>${oneObject.position}<span>Row: </span>${oneObject.row}<span>Seat: </span>${oneObject.seatNumber}</li>`);
+        //resultArray.push('<li class="result-item"><span>Zone: </span>'+oneObject.position+'<span>Row: </span>'+oneObject.row+'<span>Seat: </span>'+oneObject.seatNumber+'</li>');
+    });
+    return resultArray;
+};
+
+function getSeat(position, row, seatNumber) {
+    return seats.find(seat => seat.position === position && seat.row === row && seat.seatNumber === seatNumber);
+};
+
+function getMedian(value) {
+    const mid = Math.floor(value * 0.5) + 1;
+    if (value % 2 !== 0) {
+        return mid;
+    } else {
+        return (mid - 1 + mid) * 0.5;
+    }
+};
+
+function getSeatDistanceFromMedian(array) {
+    let rowLength = 0;
+    let currentMedian = 0;
+    let currentDistance = 0;
+
+    rowLength = seats.filter(seat => seat.row === array[0].row && seat.position === array[0].position).length;
+    currentMedian = getMedian(rowLength);
+
+    array.forEach(option => {
+        currentDistance += Math.abs(currentMedian - option.seatNumber);
+    });
+    return currentDistance;
+};
+
+function compareOptions(optionalSeats) {
+    let currentBestOption = 0;
+
+    if (optionalSeats.length > 1) {
+        let currentSumOfTier = 0;
+        let newSumOfTier = 0;
+
+        // Comparing the options' tier
+        optionalSeats[0].forEach(oneSeat => {
+            currentSumOfTier += oneSeat.tier;
+        });
+
+        optionalSeats.forEach((oneOption, index) => {
+            if (index != 0) {
+                oneOption.forEach(oneSeat => {
+                    newSumOfTier += oneSeat.tier;
+                });
+
+                if (newSumOfTier < currentSumOfTier && currentBestOption != index) {
+                    currentSumOfTier = newSumOfTier;
+                    currentBestOption = index;
+                } else if (newSumOfTier == currentSumOfTier) {
+                    const currentRow = optionalSeats[currentBestOption][0].row;
+                    const newRow = optionalSeats[index][0].row;
+
+                    if (
+                        (optionalSeats[currentBestOption][0].position === 'auditorium' && optionalSeats[index][0].position === 'auditorium')
+                        || (optionalSeats[currentBestOption][0].position === 'balcony' && optionalSeats[index][0].position === 'balcony')
+                    ) {
+                        if (newRow <= currentRow) {
+                            // Check distance from the middle
+                            const newDistance = getSeatDistanceFromMedian(optionalSeats[index]);
+                            const currentDistance = getSeatDistanceFromMedian(optionalSeats[currentBestOption]);
+
+                            if (newDistance < currentDistance && currentBestOption != index) {
+                                currentBestOption = index;
+                            }
+                        }
+                    }
+                }
+                newSumOfTier = 0;
+            }
+        });
+        return currentBestOption;
+    } else {
+        return 0;
+    }
+};
+
+function findFreeSeats() {
+    const positions = ['auditorium', 'balcony'];
+    const rows = ['A', 'B', 'C', 'D', 'E'];
+    let options = [];
+
+    positions.forEach(position => {
+        rows.forEach(row => {
+            let seatNumber = 1;
+
+            while (getSeat(position, row, seatNumber)) {
+                let currentSeat = getSeat(position, row, seatNumber);
+
+                // Finding sequences
+                if (!currentSeat.reserved) {
+                    let suitableSeats = [];
+                    suitableSeats.push(currentSeat);
+
+                    if (numberOfDemandedSeats.value > 1) {
+                        for (let i = 1; i < numberOfDemandedSeats.value; i++) {
+                            let adjacentSeat = getSeat(position, row, seatNumber + i);
+
+                            if (adjacentSeat != null && !adjacentSeat.reserved) {
+                                //Neighbour is valid
+                                suitableSeats.push(adjacentSeat);
+                                if (suitableSeats.length == numberOfDemandedSeats.value) options.push(suitableSeats);
+                            } else {
+                                suitableSeats.length = 0;
+                                break;
+                            }
+                        };
+                    } else if (numberOfDemandedSeats.value == 1) {
+                        options.push(suitableSeats);
+                    }
+                };
+                seatNumber++;
+            };
+        });
+    });
+
+    bestOption = options[compareOptions(options)];
+    resetArray();
+
+    if (!bestOption) {
+        errors = 'Apologies but at the moment there are no suitable seats! Please try again later!';
+    }
+};
+
+// Helper function, helps visualizing reserved and free seats on the consol
+function visualise() {
+    positions = ['auditorium', 'balcony'];
+    rows = ['A', 'B', 'C', 'D', 'E'];
+    positions.forEach(position => {
+        rows.forEach(row => {
+            let rowText = row + "";
+            let seatNumber = 1;
+            while (getSeat(position, row, seatNumber)) {
+                let seat = getSeat(position, row, seatNumber);
+                if (seat.reserved) {
+                    rowText += "\x1b[40m \x1b[41m" + seatNumber;
+                } else {
+                    rowText += "\x1b[40m \x1b[42m" + seatNumber;
+                }
+                seatNumber++;
+            }
+            rowText += "\x1b[40m;"
+            console.log(rowText);
+        });
+    });
+};
+// visualise();
